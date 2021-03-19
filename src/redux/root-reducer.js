badc7b8fc21a2fdd3081d121c5a4c292cb092cb5
Danilo <dan.buchner@gmail.com>
2021-03-19 16:18:33 -0400

    Added Redux, redux-logger and react-redux to the project. Also started to build the root reducer and the first rducer for the user related state

---
+++ src/redux/root-reducer.js
@@ -0,0 +1,19 @@
+/**
+ * Root reducer is the base object 
+ * that represents the state of the application
+ * 
+ * It also contains all individual reducer for 
+ * the individual functionalities and components across the app
+ * 
+ * March 19,2021
+ */
+
+import {comnbineReducers} from 'redux';
+
+import userReducer from './user/user.reducer';
+
+// Combines and export all reducers
+export default combineReducers({
+    user: userReducer
+});
+
---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,27 @@
+/**
+ * It will contain all the state related to user
+ * A reducer it's a function that have two properties state and action
+ * Action is an object with properties type and payload
+ */
+
+// Sets a initial state
+const INITIAL_STATE = {
+    currentUser : null
+}
+
+const userReducer = (state = INITIAL_STATE, action) => {
+    // action - is something that the reducestore pass to the reducer
+    // state - is the properties passed when the action gets fired
+
+    switch(action.type){
+        case 'SET_CURRENT_USER':
+            // when setting the current user state
+            return{
+                ...state,
+                currentUser: action.payload
+            };
+        default: return state;
+    }
+}
+
+export default userReducer;
