89367a78b7aa23013cac3dfb2f93a7cab894e4c0
Danilo <dan.buchner@gmail.com>
2021-03-21 20:56:06 -0400

    adding the shopping bag icon component, shopping cart dropdown and wiring them with redux.

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -11,8 +11,10 @@
 import {combineReducers} from 'redux';
 
 import userReducer from './user/user.reducer';
+import cartReducer from './cart/cart.reducer';
 
 // Combines and export all reducers
 export default combineReducers({
-    user: userReducer
+    user: userReducer,
+    cart:cartReducer
 });
